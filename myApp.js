db740db82df631f1381d8a169e20621583061397
Kevin <kabton14@gmail.com>
2023-03-16 04:51:40 +0000

    Use model.findById()

--- myApp.js
+++ myApp.js
@@ -53,7 +53,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
