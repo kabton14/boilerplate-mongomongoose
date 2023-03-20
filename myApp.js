da6d6874f48cda13eeafcbd4aec654b35c5b36e3
Kevin <kabton14@gmail.com>
2023-03-20 21:04:15 +0000

    Delete multiple dox

--- myApp.js
+++ myApp.js
@@ -91,12 +91,17 @@ const removeById = (personId, done) => {
 };
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
+  Person.deleteMany({name: nameToRemove}, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  })
 
-  done(null /*, data*/);
+  
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
+  
 
   done(null /*, data*/);
 };
